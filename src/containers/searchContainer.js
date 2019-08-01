221dc52e403ccf93a09f28493639e59d6c4b85e7
Ray <rayzlui@gmail.com>
2019-08-01 12:13:33 -0700

    change mapstatetoprops from passing boolean to object

--- src/containers/searchContainer.js
+++ src/containers/searchContainer.js
@@ -4,7 +4,7 @@ import { SearchView } from '../views/searchView';
 
 function mapStateToProps(state) {
   return {
-    intro: state.weatherData.data === null,
+    intro: state.weatherData.data,
   };
 }
 
