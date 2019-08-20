9b9b923b58b759504b75098fcdca784c44b5debc
Ray <rayzlui@gmail.com>
2019-08-20 10:59:36 -0700

    Updates

---
+++ precache-manifest.866f2643dd29e599cefcedf614980439.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af23f563102ce81ca5e82b43eed6b991",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "681d2db6a2461910ac4c",
+    "url": "/weather-app/static/css/main.16e2784a.chunk.css"
+  },
+  {
+    "revision": "23d26deda9ca0234f538",
+    "url": "/weather-app/static/js/2.cd784ba7.chunk.js"
+  },
+  {
+    "revision": "681d2db6a2461910ac4c",
+    "url": "/weather-app/static/js/main.abe4bd5c.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  }
+]);
